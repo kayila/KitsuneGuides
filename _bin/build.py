e3f3affb9923e87dda252a3472fb5f245b73bccd
Ellen Marie Dash <me@duckie.co>
2019-01-09 22:44:09 -0500

    set up script to work with Netlify.

---
+++ _bin/build.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import json
+from pathlib import Path
+import sys
+
+def load_json_file(path):
+    return json.loads(Path(path).read_text())
+
+def dump_json_file(path, obj):
+    return Path(path).write_text(json.dumps(obj))
+
+api = load_json_file('./api/api.json.template')
+guide_files = Path('.').glob("**/data.json")
+
+api['guides'] = [load_json_file(f) for f in guide_files]
+
+api_json = json.dumps(api, indent="  ")
+
+if "--stdout" in sys.argv:
+    print(api_json)
+else:
+    Path("api/api.json").write_text(api_json)
